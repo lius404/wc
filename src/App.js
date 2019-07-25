942d08d9a32d14dc9e74727bc0694ae277433ee6
Mrwang <1254225149@qq.com>
2019-07-25 11:12:37 +0800

    修复部分bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 // import logo from './logo.svg';
-<<<<<<< HEAD
 import './App.css';
-import Navbar from './Components/Navbar'
+import Navbar from './component/Navbar'
 
-=======
-import './App.css'
->>>>>>> f97cfaecbbcf486fe5ec8c2f5cdf153e51db6d17
 class App extends React.Component {
   render(){
     return (
--- src/Views/Nine/index.js
+++ src/Views/Nine/index.js
@@ -1,24 +1,22 @@
 import React,{Component} from 'react'
-import Headerbar from '../../Components/Headerbar'
-import Nine_list from '../../Components/Nine_list'
+import Headerbar from '../../component/Headerbar'
+import Nine_list from '../../component/Nine_list'
 import Nine_swiper from './Nine_swiper/index_hour'
 import Headwear from '../Nine/Nine_swiper/headwear'
 import Node from '../Nine/Nine_swiper/node'
-
-import './index.css'
 
 
 
 class Nine extends Component{
     render(){
-        return<body>
+        return<div>
             <Headerbar></Headerbar>
             <Nine_list></Nine_list>
             <div className='aaaaaaaaaa'></div>
             <Nine_swiper></Nine_swiper>
             <Headwear></Headwear>
             <Node></Node>
-        </body>
+        </div>
     }
 }
 
