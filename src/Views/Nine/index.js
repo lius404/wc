4230414434f54e4e1ae8af4a7a9f6e135ef8c8dd
Mrwang <1254225149@qq.com>
2019-07-25 11:15:24 +0800

    修复部分bug

--- src/Views/Nine/index.js
+++ src/Views/Nine/index.js
@@ -5,20 +5,19 @@ import Nine_swiper from './Nine_swiper/index_hour'
 import Headwear from '../Nine/Nine_swiper/headwear'
 import Node from '../Nine/Nine_swiper/node'
 
-import './index.css'
 
 
 
 class Nine extends Component{
     render(){
-        return<body>
+        return<div>
             <Headerbar></Headerbar>
             <Nine_list></Nine_list>
             <div className='aaaaaaaaaa'></div>
             <Nine_swiper></Nine_swiper>
             <Headwear></Headwear>
             <Node></Node>
-        </body>
+        </div>
     }
 }
 
